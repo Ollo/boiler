4113139b453e94e114086b20122053fedafc1ea0
Ollo <joshua@ollomedia.com>
2013-06-14 12:32:20 -0500

    js cleanup

--- functions.php
+++ functions.php
@@ -42,6 +42,10 @@ function boiler_setup() {
 endif; // boiler_setup
 add_action( 'after_setup_theme', 'boiler_setup' );
 
+// Nooph generator
+remove_action('wp_head', 'wp_generator');
+
+
 
 /**
  * Register widgetized area and update sidebar with default widgets
@@ -62,21 +66,20 @@ add_action( 'widgets_init', 'boiler_widgets_init' );
  * Enqueue scripts and styles
  */
 function boiler_scripts() {
-	wp_enqueue_style( 'boiler-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'boiler-init', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'boiler-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_style( 'boiler-style', get_template_directory_uri() . '/css/main.css');
 
-	wp_enqueue_script( 'boiler-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script ('jquery' , array(), '', true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'boiler-plugins', get_template_directory_uri() . '/js/plugins.js', array(), '20120206', true );
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'boiler-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
+	wp_enqueue_script( 'boiler-main', get_template_directory_uri() . '/js/main.js', array(), '20120205', true );
+
 }
 add_action( 'wp_enqueue_scripts', 'boiler_scripts' );
+
+
 
 /**
  * Custom template tags for this theme.
--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,53 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+/**
+ * navigation.js
+ *
+ * Handles toggling the navigation menu for small screens.
+ */
+( function() {
+    var container = document.getElementById( 'site-navigation' ),
+        button    = container.getElementsByTagName( 'h1' )[0],
+        menu      = container.getElementsByTagName( 'ul' )[0];
+
+    if ( undefined == button || undefined == menu )
+        return false;
+
+    button.onclick = function() {
+        if ( -1 == menu.className.indexOf( 'nav-menu' ) )
+            menu.className = 'nav-menu';
+
+        if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+            button.className = button.className.replace( ' toggled-on', '' );
+            menu.className = menu.className.replace( ' toggled-on', '' );
+            container.className = container.className.replace( 'main-small-navigation', 'navigation-main' );
+        } else {
+            button.className += ' toggled-on';
+            menu.className += ' toggled-on';
+            container.className = container.className.replace( 'navigation-main', 'main-small-navigation' );
+        }
+    };
+
+    // Hide menu toggle button if menu is empty.
+    if ( ! menu.childNodes.length )
+        button.style.display = 'none';
+} )();
+
+// keyboard-image-navigation.js
+
+jQuery( document ).ready( function( $ ) {
+    $( document ).keydown( function( e ) {
+        var url = false;
+        if ( e.which == 37 ) {  // Left arrow key code
+            url = $( '.previous-image a' ).attr( 'href' );
+        }
+        else if ( e.which == 39 ) {  // Right arrow key code
+            url = $( '.entry-attachment a' ).attr( 'href' );
+        }
+        if ( url && ( !$( 'textarea, input' ).is( ':focus' ) ) ) {
+            window.location = url;
+        }
+    } );
+} );
