02247db88d93a06f98642c270afe62ca44707487
Ollo <joshua@ollomedia.com>
2013-06-26 10:13:38 -0500

    parent class function to menus

--- functions.php
+++ functions.php
@@ -34,6 +34,26 @@ function boiler_setup() {
 		'primary' => __( 'Primary Menu', 'boiler' ),
 	) );
 
+	// add parent class to menu items 
+	add_filter( 'wp_nav_menu_objects', 'add_menu_parent_class' );
+	function add_menu_parent_class( $items ) {
+	
+		$parents = array();
+		foreach ( $items as $item ) {
+			if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+				$parents[] = $item->menu_item_parent;
+			}
+		}
+		
+		foreach ( $items as $item ) {
+			if ( in_array( $item->ID, $parents ) ) {
+				$item->classes[] = 'parent-item'; 
+			}
+		}
+		
+		return $items;    
+	}
+
 	/**
 	 * Enable support for Post Formats
 	 */
